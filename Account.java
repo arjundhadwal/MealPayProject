52465b5b018130c8f43b8af46e8c9ab2653ae482
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-02-13 10:21:31 -0800

    Transactions mostly completed

--- Account.java
+++ Account.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class Account {
     private String name = "";
+    private int thisID = 0;
     private static int idNumber = 0;
     private double balance = 0;
-    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>;
+    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+
+    //constructors
+    public Account(String myName){
+        thisID = idNumber;
+        idNumber++;
+        name = myName;
+        balance = 0;
+    }
     
 }
--- Transaction.java
+++ Transaction.java
@@ -3,5 +3,32 @@ public class Transaction{
 private int month = 0;
 private int day = 0;
 private double amount = 0;
-private studentIDNum = 0;
+private int studentIDNum = 0;
+
+//constructor
+public Transaction(int myMonth, int myDay, double myAmount, int myID){
+    month = myMonth; day = myDay; amount = myAmount; studentIDNum = myID;
+}
+
+//methods
+
+public int getDay(){
+    return day;
+}
+
+public int getMonth(){
+    return month;
+}
+
+public double getAmount(){
+    return amount;
+}
+
+public int getPurchaserID(){
+    return studentIDNum;
+}
+
+public String toString(){
+    return ("("+month+"/"+day+"), Amount: "+amount+" Purchaser ID: "+studentIDNum);
+}
 }
