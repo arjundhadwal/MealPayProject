62cfd96049ebad30fa4ec0dda5427e15aa8beddc
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-02-13 10:05:26 -0800

    Initial files

---
+++ Account.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Account {
+    private String name = "";
+    private static int idNumber = 0;
+    private double balance = 0;
+    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>;
+    
+}
---
+++ Transaction.java
@@ -0,0 +1,7 @@
+public class Transaction{
+//fields
+private int month = 0;
+private int day = 0;
+private double amount = 0;
+private studentIDNum = 0;
+}
